059106925ff8c929d998a7e06f0b6b35d8340009
Nathan-Metko-UMN <metko010@umn.edu>
2026-05-29 13:02:49 -0500

    change service and startup to work with display

--- startup.sh
+++ startup.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+export DISPLAY=:1
+export XAUTHORITY=/home/northstar_agx/.Xauthority
+
+# WAIT for the physical X11 display socket to exist before continuing
+while [ ! -e /tmp/.X11-unix/X1 ]; do
+  echo "Waiting for Graphical Desktop to load..."
+  sleep 2
+done
+
 arducam="/dev/v4l/by-id/usb-Arducam_Arducam_B0495__USB3_2.3MP__Arducam_202500915_0001-video-index0"
 cam1="/dev/v4l/by-id/usb-Intel_R__RealSense_TM__Depth_Camera_435_Intel_R__RealSense_TM__Depth_Camera_435_825513025416-video-index0"
 cam2="/dev/v4l/by-id/usb-Intel_R__RealSense_TM__Depth_Camera_435_Intel_R__RealSense_TM__Depth_Camera_435_825513025424-video-index0"
@@ -6,6 +16,7 @@ cam3="/dev/v4l/by-id/usb-Intel_R__RealSense_TM__Depth_Camera_415_Intel_R__RealSe
 
 if [ -e "$arducam" ] && [ -e "$cam1" ] && [ -e "$cam2" ] && [ -e "$cam3" ]; then
   docker run --rm --net=host --ipc=host --pid=host --privileged \
+    -e="DISPLAY" -e="TERM" -e="QT_X11_NO_MITSHM=1" -e="COLORTERM" \
     --runtime=nvidia \
     --shm-size=8g \
     --group-add video \
@@ -15,15 +26,16 @@ if [ -e "$arducam" ] && [ -e "$cam1" ] && [ -e "$cam2" ] && [ -e "$cam3" ]; then
     -v="/opt/nvidia/vpi3:/opt/nvidia/vpi3:ro" \
     -e="NVIDIA_DRIVER_CAPABILITIES"=all \
     -e="FASTRTPS_DEFAULT_PROFILES_FILE"=/opt/ros/fastdds.xml \
+    -v="$HOME/Docker/realsense_ws/fastdds.xml:/opt/ros/fastdds.xml" \
     -v="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    -v="/home/northstar_agx/Docker/realsense_ws/fastdds.xml:/opt/ros/fastdds.xml" \
-    -v="/home/northstar_agx/Docker/realsense_ws:/home/ubuntu/realsense_ws" \
+    -v="${HOME}/.Xauthority:/home/ubuntu/.Xauthority" \
+    -v="${HOME}/Docker/realsense_ws:/home/ubuntu/realsense_ws" \
     -w="/home/ubuntu/realsense_ws" \
+    --user=$(id -u):$(id -g) \
     --name="ros-container" \
-    -v="/home/northstar_agx/.tar-installs:/devtools" \
-    -v="/home/northstar_agx/.config/helix:/home/ubuntu/.config/helix" \
+    -v="${HOME}/.tar-installs:/devtools" \
+    -v="${HOME}/.config/helix:/home/ubuntu/.config/helix" \
     sentry:latest bash -c "source /opt/ros/humble/setup.bash && ./run.sh localization"
-    # --user=1000:1000 \
 else
   echo "A camera is not plugged in"
 fi
